ecea0c021d0f93be5b548997d011cff8245cffe3
Dan Rammer <hamersaw@bushpath.com>
2016-11-05 20:58:34 -0600

    generalized datatypes of http probe results table.

--- src/writer.rs
+++ src/writer.rs
@@ -13,7 +13,7 @@ CREATE TABLE http_probes (
     attempts_to_declare_failure         INT NULL,
     domain                              TEXT NOT NULL,
     port                                INT NULL,
-    url_suffix                          TEST NULL,
+    url_suffix                          TEXT NULL,
     follow_redirect                     BOOL NULL,
     PRIMARY KEY(probe_id)
 )";
@@ -32,15 +32,15 @@ INSERT INTO http_probes (
 
 const CREATE_HTTP_PROBE_RESULTS_TABLE_STMT: &'static str = "
 CREATE TABLE http_probe_results (
-    probe_id                            UNSIGNED BIG INT NOT NULL,
-    prober_hostname                     VARCHAR NOT NULL,
-    timestamp_sec                       UNSIGNED BIG INT NOT NULL,
+    probe_id                            INT NOT NULL,
+    prober_hostname                     TEXT NOT NULL,
+    timestamp_sec                       INT NOT NULL,
     success                             BOOLEAN NOT NULL,
-    error_message                       VARCHAR(255) NULL,
-    application_layer_latency_nanosec   UNSIGNED BIG INT NULL,
-    http_status_code                    MEDIUMINT NULL,
-    http_status_message                 VARCHAR(255) NULL,
-    application_bytes_received          MEDIUMINT NULL,
+    error_message                       TEXT NULL,
+    application_layer_latency_nanosec   INT NULL,
+    http_status_code                    INT NULL,
+    http_status_message                 TEXT NULL,
+    application_bytes_received          INT NULL,
     PRIMARY KEY(probe_id, timestamp_sec)
 )";
 
