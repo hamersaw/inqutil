719fde08cfa04ed01dba392b15c614b58bd6fc43
Dan Rammer <hamersaw@bushpath.com>
2016-12-13 12:10:30 -0700

    implemented print writer.

---
+++ src/writer/print.rs
@@ -0,0 +1,29 @@
+use writer::Writer;
+
+use pb::proddle::{Probe, ProbeResult};
+
+pub struct PrintWriter {
+}
+
+impl PrintWriter {
+    pub fn new() -> PrintWriter {
+        PrintWriter {
+        }
+    }
+}
+
+impl Writer for PrintWriter {
+    fn write_probe(&self, probe: Probe) -> Result<(), String> {
+        println!("probe: {:?}", probe);
+        Ok(())
+    }
+
+    fn write_probe_result(&self, probe_result: ProbeResult) -> Result<(), String> {
+        println!("probe_result: {:?}", probe_result);
+        Ok(())
+    }
+
+    fn close(&self) -> Result<(), String> {
+        Ok(())
+    }
+}
