85ec38df2285c9e1a5f02efd9a3196cdb29b5df0
Dan Rammer <hamersaw@bushpath.com>
2016-12-14 09:40:10 -0700

    changed cli options. differentiated between init and write operations. changed print to stdout.

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,31 @@ const USAGE: &'static str = "
 Application to parse probe result protobuf files
 
 Usage:
-    inqutil postgresql <username> (--probe | --result) <filename>
-    inqutil print (--probe | --result) <filename>
-    inqutil sqlite3 <db-filename> (--probe | --result) <filename>
-    inquitl (-h | --help)
+    inqutil init (--postgresql <username> | --sqlite3 <db-filename>)
+    inqutil write (--probe | --result) <filename> (--postgresql <username> | --stdout | --sqlite3 <db-filename>) 
+    inqutil (-h | --help)
 
 Options:
+    --postgresql            Specify postgresql database as output.
+    --probe                 Specify proddle probe file as input.
+    --result                Specify proddle probe result file as input.
+    --sqlite3               Specify sqlite3 database as output.
+    --stdout                Specify stdout as output.
     -h --help               Show this screen.
 ";
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
-    cmd_postgresql: bool,
-    cmd_print: bool,
-    cmd_sqlite3: bool,
+    cmd_init: bool,
+    cmd_write: bool,
     arg_db_filename: String,
     arg_filename: String,
     arg_username: String,
+    flag_postgresql: bool,
     flag_probe: bool,
     flag_result: bool,
     flag_sqlite3: bool,
+    flag_stdout: bool,
 }
 
 fn main() {
@@ -49,56 +54,60 @@ fn main() {
                         .and_then(|d| d.decode())
                         .unwrap_or_else(|e| e.exit());
 
-    //open writer
-    let writer: Box<Writer> = if args.cmd_postgresql {
-        //open postgresql writer
-        let writer = match PostgresqlWriter::open() {
-            Ok(writer) => writer,
+    if args.cmd_init {
+
+    } else if args.cmd_write {
+        //open writer
+        let writer: Box<Writer> = if args.flag_postgresql {
+            //open postgresql writer
+            let writer = match PostgresqlWriter::open() {
+                Ok(writer) => writer,
+                Err(e) => panic!("{}", e),
+            };
+
+            Box::new(writer) as Box<Writer>
+        } else if args.flag_stdout {
+            //open print writer
+            Box::new(PrintWriter::new()) as Box<Writer>
+        } else if args.flag_sqlite3 {
+            //open sqlite3 writer
+            let path = Path::new(&args.arg_db_filename);
+            let writer = match Sqlite3Writer::open(path) {
+                Ok(writer) => writer,
+                Err(e) => panic!("{}", e),
+            };
+
+            Box::new(writer) as Box<Writer>
+        } else {
+            panic!("unknown writer type");
+        };
+
+        //open file for reading
+        let mut file = match File::open(args.arg_filename) {
+            Ok(file) => file,
             Err(e) => panic!("{}", e),
         };
 
-        Box::new(writer) as Box<Writer>
-    } else if args.cmd_print {
-        //open print writer
-        Box::new(PrintWriter::new()) as Box<Writer>
-    } else if args.cmd_sqlite3 {
-        //open sqlite3 writer
-        let path = Path::new(&args.arg_db_filename);
-        let writer = match Sqlite3Writer::open(path) {
-            Ok(writer) => writer,
-            Err(e) => panic!("{}", e),
-        };
-
-        Box::new(writer) as Box<Writer>
-    } else {
-        panic!("unknown writer type");
-    };
-
-    //open file for reading
-    let mut file = match File::open(args.arg_filename) {
-        Ok(file) => file,
-        Err(e) => panic!("{}", e),
-    };
-
-    //iterate over records
-    if args.flag_probe {
-        let probe_reader = ProbeReader::new(&mut file);
-        for probe in probe_reader {
-            if let Err(e) = writer.write_probe(probe) {
-                println!("{}", e);
+        //iterate over records
+        if args.flag_probe {
+            let probe_reader = ProbeReader::new(&mut file);
+            for probe in probe_reader {
+                if let Err(e) = writer.write_probe(probe) {
+                    println!("{}", e);
+                }
+            }
+        } else if args.flag_result {
+            let probe_result_reader = ProbeResultReader::new(&mut file);
+            for probe_result in probe_result_reader {
+                if let Err(e) = writer.write_probe_result(probe_result) {
+                    println!("{}", e);
+                }
             }
         }
-    } else if args.flag_result {
-        let probe_result_reader = ProbeResultReader::new(&mut file);
-        for probe_result in probe_result_reader {
-            if let Err(e) = writer.write_probe_result(probe_result) {
-                println!("{}", e);
-            }
-        }
-    }
 
-    //close writer
-    if let Err(e) = writer.close() {
-        panic!("{}", e);
+        //close writer
+        if let Err(e) = writer.close() {
+            panic!("{}", e);
+        }
     }
 }
